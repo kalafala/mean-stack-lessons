f5dcf5af7497dce82dabaaca0348286083cb6cdb
KERIM KALAFALA <kerimkalafala@KERIMs-MacBook-Pro.local>
2016-03-11 16:16:36 -0500

    Additional mysql integration

--- mysql_server.js
+++ mysql_server.js
@@ -4,6 +4,19 @@ var express = require('express'),
 	port = process.env.PORT || 3002;
 
 var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host  : 'localhost',
+	user  : 'root',
+	password : 'hello',
+	database : 'mytest'
+});
+connection.connect();
+
+connection.query('SELECT * from mytable', function(err, rows, fields) {
+	if (err) throw err;
+	console.log('Solution: ', rows);
+}); 
+
 
 // Simple route handler example
 app.get('/getRecords',function(req,res,next) {
